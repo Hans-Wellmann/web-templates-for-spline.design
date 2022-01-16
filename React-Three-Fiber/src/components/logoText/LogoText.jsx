be176f34a4740a2021b2d7a9f25ca5327c100524
clickonrefresh <clickonrefresh@gmail.com>
2022-01-16 15:49:17 +0200

    page routing fix

--- React-Three-Fiber/src/components/logoText/LogoText.jsx
+++ React-Three-Fiber/src/components/logoText/LogoText.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import {  withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
 
 const Container = styled.div`
